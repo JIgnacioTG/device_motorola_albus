4a257ab588ad1ce20b1e413fd856511f90660729
JIgnacioTG <jitg97@hotmail.com>
2018-04-23 18:42:12 +0000

    albus: Atomic-OS bringup

--- aos.mk
+++ aos.mk
@@ -18,8 +18,12 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, device/motorola/albus/full_albus.mk)
 
-# Inherit some common lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common AOSP stuff.
+TARGET_ARCH := arm64
+TARGET_DENSITY := xxhdpi
+TARGET_BOOT_ANIMATION_RES := 1080
+
+$(call inherit-product, vendor/aos/config/common.mk)
 
 # Boot animation
 TARGET_SCREEN_WIDTH := 1080
@@ -27,7 +31,7 @@ TARGET_SCREEN_HEIGHT := 1920
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := albus
-PRODUCT_NAME := lineage_albus
+PRODUCT_NAME := aos_albus
 PRODUCT_BRAND := motorola
 PRODUCT_MANUFACTURER := motorola
 
